d45d8647d8d98df031a8b854f241a6decdeb4c22
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2023-06-27 18:58:40 +0300

    Attemp 5

--- src/componnets/data.ts
+++
@@ -1,7 +0,0 @@
- const  USER_DATA:{id:number,title:string,text:string,authorName:string| number, image:any}[] = [
-    { id:1,title:'Eilat Trip 2019',text:"random text",authorName:"yosi2001", image:'https://www.crazysexyfuntraveler.com/wp-content/uploads/2016/10/holidays-in-Eilat-Israel-Isrotel-Royal-Garden-Eilat-5.jpg'},
-    { id:2, title:'Tolum Trip 2009',text:"random text",authorName:"Ronit555", image:'https://www.everysteph.com/wp-content/uploads/2021/10/Tulum-ruins-in-Mexico-1.jpg'},
-    { id:3, title:'Yam Hamellah Trip',text:"random text",authorName:"Nisim15", image:'https://www.abrahamtours.com/wp-content/uploads/2019/06/DSC_0773.jpg'},
-   ];
-
-   export default USER_DATA;
