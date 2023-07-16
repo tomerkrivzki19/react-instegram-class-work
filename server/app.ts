f4a1431b3af6e2e91f7f179b33be2f1dd3a6297d
tomerkrivzki19 <133903404+tomerkrivzki19@users.noreply.github.com>
2023-07-16 21:04:33 +0300

    update class work

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,10 @@
+import { Request,Response } from "express";
+
 const express = require('express');
 const app = express();
 const port = 4000;
 const mongooes = require('mongoose')
+
 
 
 // moongoes access:
@@ -14,12 +17,22 @@ mongooes
     console.log(`at mongoose connect: ${err.message}`);
   });
 
-const userError = new TypeError("Something happened!");
-//   const RedirectionErr = new ErrorEvent("Something happened!");
-const serverErr = new SyntaxError("Something happened!");
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+//Request & Response ---> i libbary that coonect the req and responed to the type scrip.
+// the instalattion is : 
+app.get('/4xxCode', (req:Request,res:Response)=>{
+  res.render('4xxCode');
+})
+
+app.get('/5xxCode', (req:Request,res:Response)=>{
+  res.render('5xxCode');
+})
+
+app.get('/special',(req:Request,res:Response)=>{
+  res.render('Special');
+});
 
 
 app.get('/', (req, res) => {
